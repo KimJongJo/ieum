4db8ee262cf810a4b0bc3831e8eb1fa6977fd64e
devsolpark <devsolpark@gmail.com>
2025-09-26 09:51:43 +0900

    0926_pes_controller

---
+++ src/main/java/controller/reservation/CommunityWrite.java
@@ -0,0 +1,43 @@
+package controller.reservation;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CommunityWrite
+ */
+@WebServlet("/write")
+public class CommunityWrite extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CommunityWrite() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("allCommunity/communityWrite.jsp").forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String title = request.getParameter("title");
+		String content = request.getParameter("content");
+		String categoryNostr = request.getParameter("categoryNo");
+		
+	}
+}
