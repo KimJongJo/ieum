07cc339525d20953bf87a0f22c10b73bfab4c639
leem jihae <leem.jihae@gmail.com>
2025-09-26 09:24:30 +0900

    0926 ljh dto수정2

--- src/main/java/dto/ExamDto.java
+++ src/main/java/dto/ExamDto.java
@@ -1,15 +1,15 @@
 package dto;
 
-public class SelfExamDto {
+public class ExamDto {
 	
 	Integer examNo;	// 자가진단 번호
 	String examCategory;	// 자가진단명
 	Integer fileNo;	// 파일 번호(썸네일)
 	String examInfo;	// 자가검진 설명
-	public SelfExamDto() {
+	public ExamDto() {
 		super();
 	}
-	public SelfExamDto(Integer examNo, String examCategory, Integer fileNo, String examInfo) {
+	public ExamDto(Integer examNo, String examCategory, Integer fileNo, String examInfo) {
 		super();
 		this.examNo = examNo;
 		this.examCategory = examCategory;
