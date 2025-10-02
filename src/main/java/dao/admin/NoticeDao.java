d2c1b5bc6ea21bd258daaae34e094b02814edbf5
devsolpark <devsolpark@gmail.com>
2025-10-02 09:03:07 +0900

    1002 pes 공지사항관리 구현중

---
+++ src/main/java/dao/admin/NoticeDao.java
@@ -0,0 +1,22 @@
+package dao.admin;
+
+import java.util.List;
+
+import dto.NoticeDto;
+
+
+public interface NoticeDao {
+	public void insert(NoticeDto notice) throws Exception;
+
+	public NoticeDto select(Integer dNo) throws Exception;
+
+	public Integer cnt() throws Exception;
+
+	public List<NoticeDto> selectNoticeList(Integer uNo, String keyword, String sort, Integer row) throws Exception;
+
+	public NoticeDto update(NoticeDto notice) throws Exception;
+
+	public void delete(Integer dNo) throws Exception;
+	
+	public String selectUserNm(Integer uNo) throws Exception;
+}
